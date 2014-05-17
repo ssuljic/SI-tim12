f1034625a9eca2ff9d3620c7f1dcb206c759e0e3
semichamza <semichamza@gmail.com>
2014-05-17 16:03:01 +0200

    Inicijalni Maven projekat

---
+++ bakersoft/src/main/java/ba/etf/unsa/si/tim12/bakersoft/App.java
@@ -0,0 +1,13 @@
+package ba.etf.unsa.si.tim12.bakersoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
