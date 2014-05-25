9a4233a77070201e7e718928608ee7f5f96d4fe0
AmerPandzo <amerpandzo53@gmail.com>
2014-05-25 14:20:55 +0200

    Izmjenjen Controller za Potvrdu Dostave

--- bakersoft/src/main/java/controllers/DostavljacPotvrdaDostaveController.java
+++ bakersoft/src/main/java/controllers/DostavljacPotvrdaDostaveController.java
@@ -4,6 +4,8 @@ import javax.persistence.Table;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 import controllers.DostavljacIzbornikController;
@@ -21,6 +23,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.List;
+import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import utilities.JTableItem;
 import views.DostavljacPotvrdaDostaveJPanel;
@@ -32,20 +39,21 @@ public class DostavljacPotvrdaDostaveController {
         this.dostavljacPotvrdaDostaveJPanel = dostavljacPotvrdaDostaveJPanel;
     }
    
-/*
-    public ItemListener getDostavljacPotvrdaDostaveZaJTableItemListener() {
-        return new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    long idSelektiraneDostave =(JTableItem)(dostavljacPotvrdaDostaveJPanel.getDostaveJTable().getSelectedRow()).getId();
+
+    public ListSelectionListener getDostavljacPotvrdaDostaveZaJTableItemListener() {
+        return new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				// TODO Auto-generated method stub
+				if (e.getFirstIndex() == ItemEvent.SELECTED) {
+                    long idSelektiraneDostave =(long)(dostavljacPotvrdaDostaveJPanel.getDostaveJTable().getSelectedRow());
                     Baza baza = Baza.getBaza();
                     List<Dostava> sveDostave= baza.dajSve(Dostava.class);
                     dostavljacPotvrdaDostaveJPanel.popuniSaPodacima(sveDostave, idSelektiraneDostave);              
                     
                 }
-            }
-        };
-    }*/
+			}
+		};
 }
-
+}
