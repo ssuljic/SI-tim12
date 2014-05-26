4aebd66ef0c63c3ee2d3fbde98c3c49663cb5963
merimaziko <korisnik@korisnik-PC>
2014-05-26 14:35:25 +0200

    Probno

---
+++ bakersoft/src/main/java/controllers/DostavljacPreuzimanjePecivaController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import views.DostavljacPreuzimanjePecivaJPanel;
+
+public class DostavljacPreuzimanjePecivaController {
+
+		private DostavljacPreuzimanjePecivaJPanel dostavljacPreuzimanjePecivaJPanel;
+		
+		public DostavljacPreuzimanjePecivaController(DostavljacPreuzimanjePecivaJPanel dostavljacPreuzimanjePecivaJPanel)
+		{
+			this.dostavljacPreuzimanjePecivaJPanel=dostavljacPreuzimanjePecivaJPanel;
+		}
+		
+}
