1309e7cfc04d3b55cdaa525ccad6aa62518796c3
mziko <merima.ziko@gmail.com>
2014-05-29 23:02:54 +0200

    Rjesavanje konflikta xD

--- bakersoft/src/main/java/views/DostavljacPreuzimanjePecivaJPanel.java
+++ bakersoft/src/main/java/views/DostavljacPreuzimanjePecivaJPanel.java
@@ -14,50 +14,69 @@ public class DostavljacPreuzimanjePecivaJPanel extends JPanel {
     private JButton ukloniPecivoJButton;
     private JButton dodajPecivoJButton;
     private JButton preuzmiDostavuJButton;
-    private JScrollPane dostavaJScrollPane;
-    private JTextField kolicinaJTextField;
 
     /**
      * Create the panel.
      */
     public DostavljacPreuzimanjePecivaJPanel() {
         setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Preuzimanje peciva:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-        setLayout(null);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[]{0, 0, 0};
+        gridBagLayout.rowHeights = new int[]{0, 0, 300, 76, 0};
+        gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+        setLayout(gridBagLayout);
 
         JLabel nazivDostaveJLabel = new JLabel("Naziv dostave:");
-        nazivDostaveJLabel.setBounds(6, 18, 72, 14);
-        add(nazivDostaveJLabel);
+        GridBagConstraints gbc_nazivDostaveJLabel = new GridBagConstraints();
+        gbc_nazivDostaveJLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_nazivDostaveJLabel.anchor = GridBagConstraints.EAST;
+        gbc_nazivDostaveJLabel.gridx = 0;
+        gbc_nazivDostaveJLabel.gridy = 0;
+        add(nazivDostaveJLabel, gbc_nazivDostaveJLabel);
 
         nazivDostaveJTextField = new JTextField();
-        nazivDostaveJTextField.setBounds(83, 15, 169, 20);
-        add(nazivDostaveJTextField);
+        
+        
+        GridBagConstraints gbc_nazivDostaveJTextField = new GridBagConstraints();
+        gbc_nazivDostaveJTextField.insets = new Insets(0, 0, 5, 0);
+        gbc_nazivDostaveJTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_nazivDostaveJTextField.gridx = 1;
+        gbc_nazivDostaveJTextField.gridy = 0;
+        add(nazivDostaveJTextField, gbc_nazivDostaveJTextField);
         nazivDostaveJTextField.setColumns(10);
-        
-                dostavaJScrollPane = new JScrollPane();
-                dostavaJScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-                dostavaJScrollPane.setBounds(6, 166, 474, 213);
-                add(dostavaJScrollPane);
-                
-                        dostavaJTable = new JTable();
-                        dostavaJTable.setModel(new DefaultTableModel(
-                                new Object[][]{
-                                },
-                                new String[]{
-                                        "Pecivo", "\u0160ifra", "Koli\u010Dina peciva"
-                                }
-                        ));
-                        dostavaJScrollPane.setViewportView(dostavaJTable);
-        
-        
+
+        JScrollPane dostavaJScrollPane = new JScrollPane();
+        GridBagConstraints gbc_dostavaJScrollPane = new GridBagConstraints();
+        gbc_dostavaJScrollPane.insets = new Insets(0, 0, 5, 0);
+        gbc_dostavaJScrollPane.gridwidth = 2;
+        gbc_dostavaJScrollPane.fill = GridBagConstraints.BOTH;
+        gbc_dostavaJScrollPane.gridx = 0;
+        gbc_dostavaJScrollPane.gridy = 2;
+        add(dostavaJScrollPane, gbc_dostavaJScrollPane);
+
+        dostavaJTable = new JTable();
+        dostavaJTable.setModel(new DefaultTableModel(
+                new Object[][]{
+                },
+                new String[]{
+                        "Pecivo", "\u0160ifra", "Koli\u010Dina peciva"
+                }
+        ));
+        dostavaJScrollPane.setViewportView(dostavaJTable);
 
         JPanel dugmadJPanel = new JPanel();
-        dugmadJPanel.setBounds(6, 390, 474, 116);
-        add(dugmadJPanel);
+        GridBagConstraints gbc_dugmadJPanel = new GridBagConstraints();
+        gbc_dugmadJPanel.gridwidth = 2;
+        gbc_dugmadJPanel.fill = GridBagConstraints.BOTH;
+        gbc_dugmadJPanel.gridx = 0;
+        gbc_dugmadJPanel.gridy = 3;
+        add(dugmadJPanel, gbc_dugmadJPanel);
         GridBagLayout gbl_dugmadJPanel = new GridBagLayout();
         gbl_dugmadJPanel.columnWidths = new int[]{0, 0, 0};
-        gbl_dugmadJPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
+        gbl_dugmadJPanel.rowHeights = new int[]{0, 0, 0};
         gbl_dugmadJPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-        gbl_dugmadJPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_dugmadJPanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
         dugmadJPanel.setLayout(gbl_dugmadJPanel);
 
         ukloniPecivoJButton = new JButton("Ukloni pecivo");
@@ -82,36 +101,15 @@ public class DostavljacPreuzimanjePecivaJPanel extends JPanel {
         gbc_dodajPecivoJButton.gridx = 1;
         gbc_dodajPecivoJButton.gridy = 0;
         dugmadJPanel.add(dodajPecivoJButton, gbc_dodajPecivoJButton);
-                        
-                                preuzmiDostavuJButton = new JButton("Preuzmi dostavu");
-                                GridBagConstraints gbc_preuzmiDostavuJButton = new GridBagConstraints();
-                                gbc_preuzmiDostavuJButton.insets = new Insets(0, 0, 5, 0);
-                                gbc_preuzmiDostavuJButton.fill = GridBagConstraints.HORIZONTAL;
-                                gbc_preuzmiDostavuJButton.gridwidth = 2;
-                                gbc_preuzmiDostavuJButton.gridx = 0;
-                                gbc_preuzmiDostavuJButton.gridy = 1;
-                                dugmadJPanel.add(preuzmiDostavuJButton, gbc_preuzmiDostavuJButton);
-                                
-                                JLabel pecivoJLabel = new JLabel("Pecivo:");
-                                pecivoJLabel.setBounds(42, 46, 46, 14);
-                                add(pecivoJLabel);
-                                
-                                JComboBox comboBox = new JComboBox();
-                                comboBox.setBounds(83, 43, 169, 20);
-                                add(comboBox);
-                                
-                                JLabel kolicinaJLabel = new JLabel("Kolicina:");
-                                kolicinaJLabel.setBounds(42, 81, 46, 14);
-                                add(kolicinaJLabel);
-                                
-                                kolicinaJTextField = new JTextField();
-                                kolicinaJTextField.setBounds(83, 78, 169, 20);
-                                add(kolicinaJTextField);
-                                kolicinaJTextField.setColumns(10);
-                                
-                                JButton btnDodajPecivo = new JButton("Dodaj pecivo");
-                                btnDodajPecivo.setBounds(122, 119, 130, 23);
-                                add(btnDodajPecivo);
+
+        preuzmiDostavuJButton = new JButton("Preuzmi dostavu");
+        GridBagConstraints gbc_preuzmiDostavuJButton = new GridBagConstraints();
+        gbc_preuzmiDostavuJButton.fill = GridBagConstraints.HORIZONTAL;
+        gbc_preuzmiDostavuJButton.gridwidth = 2;
+        gbc_preuzmiDostavuJButton.insets = new Insets(0, 0, 0, 5);
+        gbc_preuzmiDostavuJButton.gridx = 0;
+        gbc_preuzmiDostavuJButton.gridy = 1;
+        dugmadJPanel.add(preuzmiDostavuJButton, gbc_preuzmiDostavuJButton);
 
         
     }
