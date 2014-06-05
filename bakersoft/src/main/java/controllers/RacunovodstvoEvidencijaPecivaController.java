79e2b92b262b5a316e0072704b8f8c0a573f5af1
AmerPandzo <amerpandzo53@gmail.com>
2014-06-05 14:26:07 +0200

    Closed issue #54
    Sada je nemoguce dodati 2 peciva sa istom sifrom .

--- bakersoft/src/main/java/controllers/RacunovodstvoEvidencijaPecivaController.java
+++ bakersoft/src/main/java/controllers/RacunovodstvoEvidencijaPecivaController.java
@@ -3,6 +3,8 @@
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import utilities.Baza;
 import utilities.JComboBoxItem;
@@ -14,6 +16,7 @@ import javax.swing.table.DefaultTableModel;
 import entities.Klijent;
 import entities.Pecivo;
 import entities.Racun;
+import utilities.Baza;
 
 
 
@@ -30,9 +33,24 @@ public class RacunovodstvoEvidencijaPecivaController
 		return new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				try {
+					boolean postoji_pecivo=false;
+					Baza baza = Baza.getBaza();
+					List<Pecivo> _svapeciva = baza.dajSve(Pecivo.class);
 					racunovodstvoEvidencijaPecivaJPanel.dodajPecivo();
+					
+					 for(Pecivo p : _svapeciva) {
+					    	if(p.getSifra().equals(racunovodstvoEvidencijaPecivaJPanel.getSifratextField().getText())) {
+					    		postoji_pecivo = true;
+					    		break;
+					    	}
+					 }
+					 if(!postoji_pecivo) {
 	                JOptionPane.showMessageDialog(racunovodstvoEvidencijaPecivaJPanel.getParent(), "Pecivo je uspješno dodano.");
 					racunovodstvoEvidencijaPecivaJPanel.osvjeziPanel();
+					 }
+					 else{
+				    		JOptionPane.showMessageDialog(racunovodstvoEvidencijaPecivaJPanel.getParent(),"Pecivo sa unesenom sifrom vec postoji!");
+			    			}
 				} catch (Exception e) {
 	                JOptionPane.showMessageDialog(racunovodstvoEvidencijaPecivaJPanel.getParent(), e.getMessage());
 				}
