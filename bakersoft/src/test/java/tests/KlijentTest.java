252cb01caeb2e35939765c1539d1b2b32d54c995
semichamza <semichamza@gmail.com>
2014-06-10 00:39:44 +0200

    KlijentTest karakteri ispravljeni

--- bakersoft/src/test/java/tests/KlijentTest.java
+++ bakersoft/src/test/java/tests/KlijentTest.java
@@ -8,19 +8,19 @@ import entities.Klijent;
 
 public class KlijentTest {
 	
-	//Kapiram da je reèeno da se ne testiraju getteri i setteri, ali ovi su bitni jer je validacija 
-	//sadržana u njima, tj u ova dva xD ... samo æemo testirat nepravilan unos
+	//Kapiram da je receno da se ne testiraju getteri i setteri, ali ovi su bitni jer je validacija 
+	//sadrzana u njima, tj u ova dva xD ... samo cemo testirat nepravilan unos
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testSetIme() {
 		Klijent k = new Klijent();
-		k.setIme("");//prazan string pokušavamo
+		k.setIme("");//prazan string pokusavamo
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testSetTelefon() {
 		Klijent k = new Klijent();
-		k.setTelefon("");//prazan string pokušavamo
+		k.setTelefon("");//prazan string pokusavamo
 	}
 
 }
